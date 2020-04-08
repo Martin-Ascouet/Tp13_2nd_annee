c1f5e22a95bc612a34ae20c0c3130e65dd77e920
Martin-Ascouet <martin.ascouet@isen-ouest.yncrea.fr>
2020-04-08 16:58:20 +0200

    .

--- BinaryTree.py
+++ BinaryTree.py
@@ -6,13 +6,4 @@ class BinaryTree:
     def GetRoot(self):
         return self.__root
 
-"""
-    def isRoot(self, node):
-        return True if node == self.__root else False
 
-    def size(self, node):
-        if node.GetRight() == None and node.GetLeft() == None:
-            return
-        else:
-            return 1 + self.size(se)
-"""
